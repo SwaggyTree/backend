6b4d266a28c8b4794595fad67c3f553532524200
SwaggyTree <leon_pablo@dlsu.edu.ph>
2024-04-07 23:21:46 +0800

    frontend

---
+++ models/postModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    body: {
+        type: String,
+        required: true
+    },
+
+    tag: {
+        type: String,
+        required: true
+    },
+
+    user: {
+        type: String,
+        required: true
+    }
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('Post', postSchema)
+
---
+++ routes/posts.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const {
+    createPost,
+    getPosts,
+    getPost,
+    deletePost,
+    updatePost
+} = require('../controllers/postController')
+
+const router = express.Router()
+
+router.get('/', getPosts)
+
+router.get('/:id', getPost)
+
+router.post('/', createPost)
+
+router.delete('/:id', deletePost)
+
+router.patch('/:id', updatePost)
+
+module.exports = router
